1dfb9b17dca1eaf10e71c396b8cb235a18a6c300
Mamtha Tadiboina <mtadiboina@fb.com>
2023-03-13 18:10:22 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,38 +1,123 @@
 import java.util.*;
+
 public class Duke {
+    private static ArrayList<Item> itemList = new ArrayList<>();
+
     public static void main(String[] args) {
 
-        String borderLine = "____________________________________________________________";
-        String greetings = "Hello! I'm Duke \n\tWhat can I do for you";
-        System.out.printf("\t%s\n\t%s\n\t%s\n",borderLine, greetings, borderLine);
-
-        ArrayList itemList = new ArrayList();
+        printIntroductionGreeting();
         Scanner input = new Scanner(System.in);
 
         while(true) {
             System.out.print("Enter input : ");
             String userInput = input.nextLine().trim();
-            String lineItem = "";
 
-            if("list".equalsIgnoreCase(userInput)) {
-                for(int i=0; i<itemList.size(); i++) {
-                    if(!"list".equalsIgnoreCase(itemList.get(i).toString())) {
-                        lineItem += "\t" + (i + 1) + ". " + itemList.get(i);
+            switch (userInput) {
+                case "list":
+                    printItemList();
+                    break;
+                case "bye":
+                    printEndGreeting();
+                    return;
+                default:
+                    if(isItem(userInput)) {
+                        if (isItemExists(userInput) == false) {
+                            Item item = new Item(userInput, false);
+                            itemList.add(item);
+                            System.out.println("Item added");
+                        } else {
+                            System.out.println("Item already exists. Please add another item.");
+                        }
                     }
-                }
-                lineItem.trim();
-            } else {
-                lineItem = "\t" + userInput + "\n";
-                itemList.add(lineItem);
+                    String[] itemArray = userInput.split(" ", 2);
+                    if(itemArray.length > 1 && isNumeric(itemArray[1]) == true) {
+                        if(itemArray[0].equals("mark")) {
+                            markOrUnmarkItem(Integer.parseInt(itemArray[1]), true);
+                        }
+                        if(itemArray[0].equals("unmark")) {
+                            markOrUnmarkItem(Integer.parseInt(itemArray[1]), false);
+                        }
+                    }
+                    break;
             }
-
-            if("bye".equalsIgnoreCase(userInput)) {
-                String message = "Bye. Hope to see you again soon!";
-                System.out.printf("\t%s\n\t%s\n\t%s\n",borderLine, message, borderLine);
-                break;
-            }
-
-            System.out.printf("\t%s\n%s\t%s\n",borderLine, lineItem, borderLine);
         }
+    }
+
+    public static void printIntroductionGreeting() {
+        String borderLine = "____________________________________________________________";
+        String greetings = "Hello! I'm Duke \n\tWhat can I do for you";
+        System.out.printf("\t%s\n\t%s\n\t%s\n",borderLine, greetings, borderLine);
+    }
+    public static void printEndGreeting() {
+        String borderLine = "____________________________________________________________";
+        String greetings = "Bye. Hope to see you again soon!";
+        System.out.printf("\t%s\n\t%s\n\t%s\n",borderLine, greetings, borderLine);
+    }
+
+    public static Boolean isItem(String input) {
+        if(input.contains("list") || input.contains("mark") || input.contains("unmark") || input.equals(" ")) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isItemExists(String item) {
+        for(int i=0; i<itemList.size(); i++) {
+            if(itemList.get(i).getItemName().equalsIgnoreCase(item)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static String getStatusSymbol(boolean status) {
+        if(status == true) {
+            return "X";
+        }
+        return " ";
+    }
+
+    private static void printItemList() {
+        String borderLine = "\t____________________________________________________________";
+        System.out.println(borderLine);
+        for(int i=0; i<itemList.size(); i++) {
+            if(isItem(itemList.get(i).getItemName())) {
+                System.out.println("\t" + (i + 1) + ". [" + getStatusSymbol(itemList.get(i).getStatus()) + "] " + itemList.get(i).getItemName() + "\t");
+            }
+        }
+        System.out.println(borderLine);
+    }
+
+    private static void printMarkOrUnmarkItem(Boolean status) {
+        if(status == true) {
+            System.out.println("\tNice! I've marked this task as done:");
+        } else {
+            System.out.println("\tOK, I've marked this task as not done yet:");
+        }
+    }
+
+    private static void markOrUnmarkItem(Integer itemNo, Boolean status) {
+        String borderLine = "\t____________________________________________________________";
+        System.out.println(borderLine);
+        for(int i=0; i<itemList.size(); i++) {
+            if(i+1 == itemNo) {
+                itemList.get(i).setStatus(status);
+                printMarkOrUnmarkItem(status);
+                System.out.println("\t [" + getStatusSymbol(itemList.get(i).getStatus()) + "] " + itemList.get(i).getItemName() + "\t");
+            }
+        }
+        System.out.println(borderLine);
+    }
+
+    public static boolean isNumeric(String itemNo) {
+        if (itemNo == " ") {
+            return false;
+        }
+        try {
+            Integer.parseInt(itemNo);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 }
