0de6be8711e0fa33b6711df59f40ea8836a5ef50
mamthat <mamtha_t@outlook.com>
2023-03-20 18:41:32 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -29,30 +29,22 @@ public class Duke {
                         }
                     }
 
-                    String[] taskArray = userInput.split(" ", 2);
-                    if(userInput.contains("mark")) {
-                        if(taskArray.length > 1 && isNumeric(taskArray[1]) == true) {
-                            if(taskArray[0].equals("mark")) {
-                                markOrUnmarkTask(Integer.parseInt(taskArray[1]), true);
-                            }
-                            if(taskArray[0].equals("unmark")) {
-                                markOrUnmarkTask(Integer.parseInt(taskArray[1]), false);
-                            }
+                        if(userInput.contains("mark") || userInput.contains("unmark")) {
+                            markOrUnmarkTask(userInput);
                         }
-                    }
+                    
+                        if(userInput.contains("todo")) {
+                            setToDoTask(userInput);
+                        }
 
-                    if(userInput.contains("todo")) {
-                        setToDoTask(taskArray[1]);
-                    }
-
-                    if(userInput.contains("deadline")) {
-                        setDeadlineTask(taskArray[1]);
-                    }
-
-                    if(userInput.contains("event")) {
-                        setEventTask(taskArray[1]);
-                    }
-
+                        if(userInput.contains("deadline")) {
+                            setDeadlineTask(userInput);
+                        }
+                    
+                        if(userInput.contains("event")) {
+                            setEventTask(userInput);
+                        }     
+                
                     break;
             }
         }
@@ -119,78 +111,133 @@ public class Duke {
         }
     }
 
-    private static void markOrUnmarkTask(Integer taskNo, Boolean status) {
-        String borderLine = "\t____________________________________________________________";
-        System.out.println(borderLine);
-        for(int i=0; i<taskList.size(); i++) {
-            if(i+1 == taskNo) {
-                if(taskList.get(i).getStatus() != status) {
-                    taskList.get(i).setStatus(status);
-                    printMarkOrUnmarkTask(status);
-                    System.out.println("\t " + taskList.get(i).toString()+ "\t");
-                } else {
-                    System.out.println("\tTask status is already set");
+    private static void markOrUnmarkTask(String userInput) {
+        try{
+            if(userInput.split(" ", 2).length > 1 && isNumeric(userInput.split(" ", 2)[1]) == false) {
+                throw new DukeException();
+            }
+
+            boolean status = false;
+            if(userInput.split(" ", 2)[0].equals("mark")) {
+                status = true;
+            }
+            else {
+                status = false;
+            }
+
+            int taskNo = Integer.parseInt(userInput.split(" ", 2)[1]);
+            String borderLine = "\t____________________________________________________________";
+            System.out.println(borderLine);
+            for(int i=0; i<taskList.size(); i++) {
+                if(i+1 == taskNo) {
+                    if(taskList.get(i).getStatus() != status) {
+                        taskList.get(i).setStatus(status);
+                        printMarkOrUnmarkTask(status);
+                        System.out.println("\t " + taskList.get(i).toString()+ "\t");
+                    } else {
+                        System.out.println("\tTask status is already set");
+                    }
                 }
             }
-        }
-        System.out.println(borderLine);
+            System.out.println(borderLine);
+        } catch(ArrayIndexOutOfBoundsException _exception) {
+            System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+        } catch(DukeException exception) {
+            System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+        } 
     }
 
-    private static void setToDoTask(String taskName) { 
-        String borderLine = "\t____________________________________________________________";
-        System.out.println(borderLine); 
+    private static void setToDoTask(String userInput) {
+        try{
+            String[] taskArray = userInput.split(" ", 2);
+            String taskName = taskArray[1];
 
-        for(int i=0; i<taskList.size(); i++) {
-            if(taskList.get(i).taskName.equalsIgnoreCase(taskName)) {
-                Task toDoTask = new Todo(taskName, true);
-                toDoTask.setStatus(taskList.get(i).getStatus()); 
-                taskList.set(i, toDoTask);
-                printTaskUpdates(toDoTask.toString());    
+            if(taskName == "") 
+                throw new DukeException();
+
+            String borderLine = "\t____________________________________________________________";
+            System.out.println(borderLine); 
+
+            for(int i=0; i<taskList.size(); i++) {
+                if(taskList.get(i).taskName.equalsIgnoreCase(taskName)) {
+                    Task toDoTask = new Todo(taskName, true);
+                    toDoTask.setStatus(taskList.get(i).getStatus()); 
+                    taskList.set(i, toDoTask);
+                    printTaskUpdates(toDoTask.toString());    
+                }
             }
-        }
 
-        System.out.println(borderLine);
+            System.out.println(borderLine);
+        } catch(ArrayIndexOutOfBoundsException _exception) {
+            System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+        } catch(DukeException exception) {
+            System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+        } 
     }
 
-    private static void setDeadlineTask(String taskInput) { 
-        String borderLine = "\t____________________________________________________________";
-        System.out.println(borderLine); 
+    private static void setDeadlineTask(String userInput) {
+        try{
+            String[] taskArray = userInput.split(" ", 2);
+            String taskInputDetails = taskArray[1];
 
-        String[] taskDetails = taskInput.split("/", 2);
-        String taskName = taskDetails[0].trim();
-        String day = taskDetails[1].split(" ")[1].trim();
+            if(taskInputDetails == "") 
+                throw new DukeException();
 
-        for(int i=0; i<taskList.size(); i++) {
-            if(taskList.get(i).taskName.equalsIgnoreCase(taskName)) {
-                Task deadlineTask = new Deadline(taskName, day);
-                deadlineTask.setStatus(taskList.get(i).getStatus()); 
-                taskList.set(i, deadlineTask);
-                printTaskUpdates(deadlineTask.toString());
-            } 
-        }
+            String borderLine = "\t____________________________________________________________";
+            System.out.println(borderLine); 
 
-        System.out.println(borderLine);
-    }
+            String[] taskDetails = taskInputDetails.split("/", 2);
+            String taskName = taskDetails[0].trim();
+            String day = taskDetails[1].split(" ")[1].trim();
 
-    private static void setEventTask(String taskInput) { 
-        String borderLine = "\t____________________________________________________________";
-        System.out.println(borderLine); 
-
-        String[] taskDetails = taskInput.split("/", 3);
-        String taskName = taskDetails[0].trim();
-        String from = taskDetails[1].trim();
-        String to = taskDetails[2].trim();
-
-        for(int i=0; i<taskList.size(); i++) {
-            if(taskList.get(i).taskName.equalsIgnoreCase(taskName)) {
-                Task eventTask = new Event(taskName, from, to);
-                eventTask.setStatus(taskList.get(i).getStatus()); 
-                taskList.set(i, eventTask);
-                printTaskUpdates(eventTask.toString());
+            for(int i=0; i<taskList.size(); i++) {
+                if(taskList.get(i).taskName.equalsIgnoreCase(taskName)) {
+                    Task deadlineTask = new Deadline(taskName, day);
+                    deadlineTask.setStatus(taskList.get(i).getStatus()); 
+                    taskList.set(i, deadlineTask);
+                    printTaskUpdates(deadlineTask.toString());
+                } 
             }
-        }
 
         System.out.println(borderLine);
+        } catch(ArrayIndexOutOfBoundsException _exception) {
+            System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.");
+        } catch(DukeException exception) {
+            System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.");
+        } 
+    }
+
+    private static void setEventTask(String userInput) {
+        try{
+            String[] taskArray = userInput.split(" ", 2);
+            String taskInputDetails = taskArray[1];
+
+            if(taskInputDetails == "") 
+                throw new DukeException();
+
+            String borderLine = "\t____________________________________________________________";
+            System.out.println(borderLine); 
+
+            String[] taskDetails = taskInputDetails.split("/", 3);
+            String taskName = taskDetails[0].trim();
+            String from = taskDetails[1].trim();
+            String to = taskDetails[2].trim();
+
+            for(int i=0; i<taskList.size(); i++) {
+                if(taskList.get(i).taskName.equalsIgnoreCase(taskName)) {
+                    Task eventTask = new Event(taskName, from, to);
+                    eventTask.setStatus(taskList.get(i).getStatus()); 
+                    taskList.set(i, eventTask);
+                    printTaskUpdates(eventTask.toString());
+                }
+            }
+
+        System.out.println(borderLine);
+        } catch(ArrayIndexOutOfBoundsException _exception) {
+            System.out.println("☹ OOPS!!! The description of an event cannot be empty.");
+        } catch(DukeException exception) {
+            System.out.println("☹ OOPS!!! The description of an event cannot be empty.");
+        } 
     }
 
 
